d79a3e449d938427d46dc50678569af84c696d2f
JK <supjk3@gmail.com>
2023-06-21 16:24:14 +0900

    1111

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import musk from './musk_deep.gif';
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={musk} width={300} border="1"/>
+      <br/><br/>
+      <h2>Docker Test</h2>
+    </div>
+  );
+}
+
+export default App;
